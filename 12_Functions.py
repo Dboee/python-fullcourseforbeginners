84a8127d5c2eafcc3fb7f77a7dbdda723a798c46
Dboee <davidboee@gmail.com>
2021-03-04 20:34:39 +0100

    Function Basics

---
+++ 12_Functions.py
@@ -0,0 +1,9 @@
+
+
+def say_hi(name): # Common practice is to lowercase and underscore function names in python
+    print("yoo " + name)
+
+
+print('================')
+say_hi("You")
+print('================')
