7a35601e3b8e7d528dc9c713c6035fc0fa680bb2
Dboee <davidboee@gmail.com>
2021-03-04 21:18:29 +0100

    IF

---
+++ 14_ifStatements.py
@@ -0,0 +1,21 @@
+is_male = True
+is_tall = False
+
+if is_male:
+    print("Hey maaan")
+    if is_tall:
+        print("You tall manperson you")
+    else:
+        print("Shortyshort short short...")
+else:
+    print("Hey giiiiirl")
+    if is_tall:
+        print("You tall womanperson you")
+    else:
+        print("Shortyshort short short...")
+
+# you also have:
+# elif
+# and
+# not()
+# or
