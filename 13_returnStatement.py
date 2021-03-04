297f17b2b70d310d559e2ce5bedf2fc57c6857c9
Dboee <davidboee@gmail.com>
2021-03-04 21:36:53 +0100

    Dictionaries

---
+++ 13_returnStatement.py
@@ -0,0 +1,9 @@
+
+def cube(num):
+    calculation = num*num*num
+    print("the results are in: ")
+    return calculation
+
+
+result = cube(4)
+print(result)
---
+++ 15_dictionaries.py
@@ -0,0 +1,19 @@
+# Key-value-pairs
+
+monthConverstions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Des": "December"
+}
+
+print(monthConverstions["Sep"])
+print(monthConverstions.get("Mar"))
