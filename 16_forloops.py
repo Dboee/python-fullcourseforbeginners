ba4addb8997ee45fafe7ad272578d892dc1f0283
Dboee <davidboee@gmail.com>
2021-03-04 22:00:26 +0100

    for loops

---
+++ 16_forloops.py
@@ -0,0 +1,15 @@
+
+friends = ["Jim", "Karen", "Kevin"]
+
+
+print("====================")
+for friend in friends:
+    for letter in friend:
+        print (letter)
+    print("====================")
+
+for index in range(1,11):
+    print(index)
+
+for i in range(len(friends)):
+    print(friends[i])
