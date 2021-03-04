48688155c6b284e240cd40cab14b6da06485520c
Dboee <davidboee@gmail.com>
2021-03-04 19:22:02 +0100

    Mad Libs Game

---
+++ 08_madLibsGame.py
@@ -0,0 +1,11 @@
+
+color = input("Enter color: ")
+pluralNoun = input("Enter plural noun: ")
+celebrity = input("Enter celebrity: ")
+
+
+print("================================================")
+print("Roses are " + color)
+print( pluralNoun + " are blue")
+print("I love " + celebrity)
+print("================================================")
