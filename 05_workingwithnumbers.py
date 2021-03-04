88dfd792d598d387557267420964c781e817b03e
Dboee <davidboee@gmail.com>
2021-03-04 17:08:48 +0100

    working with numbers

---
+++ 05_workingwithnumbers.py
@@ -0,0 +1,21 @@
+from math import *
+
+
+mynum = 365
+mynegnum = -15
+raisedtopower = 2
+needsrounding = 7.5065321
+
+
+print(mynum)
+print(abs(mynegnum))
+print(pow(mynum, raisedtopower))
+print(max(mynum, mynegnum, raisedtopower))
+print(min(mynum, mynegnum, raisedtopower))
+print(round(needsrounding))
+
+# math functions
+
+print(floor(needsrounding))
+print(ceil(needsrounding))
+print(sqrt(needsrounding))
