0de0212a02a6a99ca538b11eb27a902811636f1a
Dboee <davidboee@gmail.com>
2021-03-04 15:12:58 +0100

    Part 3

---
+++ app.py
@@ -0,0 +1,9 @@
+character_name = "Charlie"
+character_age = 70
+isMale = True
+
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
