89739ed315e80964ef3215af33b0319824b4a3a7
Dboee <davidboee@gmail.com>
2021-03-04 15:16:40 +0100

    part3 finish

--- 03_variables&datatypes.py
+++ 03_variables&datatypes.py
@@ -4,6 +4,6 @@ isMale = True
 
 
 print("There once was a man named " + character_name + ", ")
-print("he was " + character_age + " years old. ")
+print("he was " + str(character_age) + " years old. ")
 print("He really liked the name " + character_name + ", ")
-print("but didn't like being " + character_age + ".")
+print("but didn't like being " + str(character_age) + ".")
